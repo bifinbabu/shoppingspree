df1063fac4dc1844d07ae062520424cac47aa31d
bifinbabu <bifinbabu088@gmail.com>
2023-01-15 20:05:17 +0530

    Single product page updates

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
@@ -6,13 +7,70 @@ import Navbar from "../components/Navbar";
 import Newsletter from "../components/Newsletter";
 
 const Container = styled.div``;
-const Wrapper = styled.div``;
-const ImageContainer = styled.div``;
-const Image = styled.img``;
-const InfoContainer = styled.div``;
-const Title =styled.h1``
-const Desc = styled.p``
-const Price = styled.span``
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+const ImageContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+const FilterContainer = styled.div`
+  margin: 30px 0px;
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font_size: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0 5px;
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const AmountContainer = styled.div`
+display: flex;
+`;
+const Amount = styled.div``;
+const Button = styled.button``;
 
 const Product = () => {
   return (
@@ -33,6 +91,32 @@ const Product = () => {
             used to temporarily replace.
           </Desc>
           <Price>₹ 1,599</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>Add To Cart</Button>
+          </AddContainer>
         </InfoContainer>
       </Wrapper>
       <Newsletter />
