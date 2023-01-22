cc554876ec87a5e59fde8734c57092fed5d59434
bifinbabu <bifinbabu088@gmail.com>
2023-01-22 19:55:10 +0530

    (Feature) Finished cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
@@ -40,17 +41,82 @@ const Bottom = styled.div`
 const Info = styled.div`
   flex: 3;
 `;
-const Product = styled.div``
-const ProductDetail = styled.div``
-const Image = styled.img``
-const Details = styled.div``
-const PriceDetail = styled.div``
-const ProductName = styled.span``
-const ProductId = styled.span``
-const ProductColor = styled.div``
-const ProductSize = styled.span``
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ProductDetail = styled.div`
+  display: flex;
+  flex: 2;
+`;
+const Image = styled.img`
+  width: 200px;
+`;
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const ProductName = styled.span``;
+const ProductId = styled.span``;
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+const ProductSize = styled.span``;
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+const ProductAmount = styled.span`
+  font-size: 24px;
+  margin: 5px;
+`;
+const ProductPrice = styled.span`
+  font-size: 30px;
+  font-weight: 200;
+`;
 const Summary = styled.div`
   flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+const SummaryTitle = styled.h1`
+font-weight: 200;
+`;
+const SummaryItem = styled.div`
+margin: 30px 0px;
+display: flex;
+justify-content: space-between;
+font-weight: ${props => props.type === "total" && "500"};
+font-size: ${props => props.type === "total" && "24px"};
+`;
+const SummaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
+const Button = styled.button`
+width: 100%;
+padding: 10px;
+background-color: black;
+color: white;
+font-weight: 600;
 `;
 
 const Cart = () => {
@@ -74,16 +140,74 @@ const Cart = () => {
               <ProductDetail>
                 <Image src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/08f113fb-396f-4445-a89b-f82752a7cb82/air-max-90-g-golf-shoe-qlD3wL.png" />
                 <Details>
-                    <ProductName><b>Product:</b>NIke Air Max</ProductName>
-                    <ProductId><b>ID:</b>7348569</ProductId>
-                    <ProductColor />
-                    <ProductSize><b>size:</b>M</ProductSize>
+                  <ProductName>
+                    <b>Product:</b>NIke Air Max
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b>7348569
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>size:</b>9
+                  </ProductSize>
                 </Details>
               </ProductDetail>
-              <PriceDetail>Price</PriceDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>₹ 4,999</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src="https://www.leatherclue.com/image/cache/catalog/AB_TEES/Plain%20T/navy-blue-men-s-tshirt-550x550.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b>Levi's T-Shirt
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b>7348569
+                  </ProductId>
+                  <ProductColor color="blue" />
+                  <ProductSize>
+                    <b>size:</b>L
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>₹ 2,499</ProductPrice>
+              </PriceDetail>
             </Product>
           </Info>
-          <Summary>Summary</Summary>
+          <Summary>
+            <SummaryTitle>Order Summary</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Sub Total</SummaryItemText>
+              <SummaryItemPrice>₹ 2,499</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>₹ 499</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>-₹ 499</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>₹ 2,499</SummaryItemPrice>
+            </SummaryItem>
+            <Button>Checkout Now</Button>
+          </Summary>
         </Bottom>
       </Wrapper>
       <Footer />
